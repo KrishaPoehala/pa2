27983a6fe540de92d80e3a7355216e74d73377a5
Halepaa <kovaliknazario@gmail.com>
2022-12-23 22:49:13 +0200

    now the program has one entry point

--- pa2.AStar/Program.cs
+++ pa2.AStar/Program.cs
@@ -1,5 +1,6 @@
 ﻿using pa2.AStar;
 using pa2.Common;
+using pa2.Ldfs;
 
 var helper = new ConsoleHelper();
 var (queensCount, fieldSize) = helper.GetDataFromConsole();
@@ -7,9 +8,18 @@ var astar = new AStarSearch(queensCount, fieldSize);
 var generatedQueens = astar.GenerateInitialQueens();
 
 var startTime = DateTime.Now;
+Console.WriteLine("A star search: ");
 helper.Draw(generatedQueens.Select(x => x as QueenBase).ToList(), fieldSize);
 Console.WriteLine("=============================");
 var solution = astar.FindThePath(generatedQueens);
 helper.Draw(solution.Queens.Select(x => x as QueenBase).ToList(), fieldSize);
 Console.Write("The path was found in: ");
 Console.WriteLine((DateTime.Now - startTime).TotalMilliseconds);
+
+
+startTime = DateTime.Now;
+Console.WriteLine("\n Ldfs search: ");
+var ldfs = new LdfsSearch(fieldSize, queensCount);
+var ldfsSolution = ldfs.Search(ldfs.GenerateInitialQueens());
+Console.WriteLine($"The ldfs found the solution in: {(DateTime.Now - startTime).TotalMilliseconds}");
+helper.Draw(solution.Queens.Select(x => x as QueenBase).ToList(), fieldSize);
