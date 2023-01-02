d84b182bb5ce73d361e44c9f566856b692863a50
Halepaa <kovaliknazario@gmail.com>
2023-01-02 22:02:16 +0200

    added algorithm and presentation

---
+++ pa5.Lab/pa5/pa5.Algorithm/AlgorithmResults.cs
@@ -0,0 +1,11 @@
+﻿namespace pa5.Algorithm;
+
+public class AlgorithmResults
+{
+	public int Iterations = 0;
+	public int GenerationsLeft = 25;
+	public double CurrentScore = 0;
+	public double InitialShortestDistance = double.MaxValue;
+	public double ShortestDistance = 0;
+	public int TestsCount = 25;
+}
---
+++ pa5.Lab/pa5/pa5.Algorithm/ConfigurationValidation.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+
+namespace pa5.Algorithm;
+
+public class ConfigurationValidation : AbstractValidator<Configuration>
+{
+    public ConfigurationValidation()
+    {
+        RuleFor(x => x.AntsCount)
+            .GreaterThan(0)
+            .LessThanOrEqualTo(50)
+            .WithMessage($"Ants count must be in the range: from 0 to 50");
+
+        RuleFor(x => x.PointsCount)
+            .GreaterThan(0)
+            .LessThanOrEqualTo(200)
+            .WithMessage($"Points count must be in the range: from 0 to 200");
+
+        RuleFor(x => x.Ro)
+            .GreaterThanOrEqualTo(0)
+            .LessThan(5)
+            .WithMessage($"Ro must be in the range: from 0 to 5");
+
+        RuleFor(x => x.Beta)
+           .GreaterThanOrEqualTo(0)
+           .LessThan(5)
+           .WithMessage($"Beta must be in the range: from 0 to 5");
+
+        RuleFor(x => x.Alpha)
+           .GreaterThanOrEqualTo(0)
+           .LessThan(5)
+           .WithMessage($"Alpha must be in the range: from 0 to 5");
+    }
+}
